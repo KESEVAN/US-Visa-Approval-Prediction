ac7c6b57c12741dd5eff17e4374e4d9572b8a3cd
KESEVAN <kesevanbalaji@gmail.com>
2024-02-27 22:13:16 +0530

    setup_packages

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup , find_packages
+
+
+setup(
+    name="us_visa",
+    version="0.0.0",
+    author="Kesevan",
+    author_email="kesevanbalaji@gmail.com",
+    description="Prediction of US Visa Approval",
+    packages=find_packages()
+)
